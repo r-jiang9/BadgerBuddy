5a7d35bc2fd273d454bff1a1e32b0f45e040bebc
Jin Choi <yongchoi113@gmail.com>
2023-12-11 14:05:11 -0600

    many stuff: homeactivity -> mainactivity, added icons and floorplans as well as building icons in map

---
+++ app/src/main/java/com/cs407/badgerbuddy/BuildingFloorActivity.java
@@ -0,0 +1,73 @@
+package com.cs407.badgerbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+
+public class BuildingFloorActivity extends AppCompatActivity {
+
+    private ExpandableListView expandableListView;
+    private BuildingFloorListAdapter buildingFloorListAdapter;
+
+    private List<String> listDataHeader;
+    private HashMap<String, Integer> listDataChild;
+
+    private String address;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_building_floor);
+
+        String title = getIntent().getStringExtra("title");
+        String address = getIntent().getStringExtra("address");
+
+        TextView tvTitle = findViewById(R.id.tv_title);
+        TextView tvAddress = findViewById(R.id.tv_address);
+
+        tvTitle.setText(title);
+        tvAddress.setText(address);
+
+        ImageButton imageButton = findViewById(R.id.btn_back);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        // 데이터 초기화
+        initData();
+
+        // 어댑터 생성 및 설정
+        expandableListView = findViewById(R.id.expandableListView);
+        buildingFloorListAdapter = new BuildingFloorListAdapter(this, listDataHeader, listDataChild);
+        expandableListView.setAdapter(buildingFloorListAdapter);
+    }
+
+    private void initData() {
+        listDataHeader = new ArrayList<>();
+        listDataChild = new HashMap<>();
+
+        // 그룹 추가
+        listDataHeader.add("Floor P");
+        listDataHeader.add("Floor 1");
+        listDataHeader.add("Floor 2");
+        listDataHeader.add("Floor 3");
+        listDataHeader.add("Floor 4");
+
+        // 하위 항목 추가
+        listDataChild.put(listDataHeader.get(0), R.drawable.memu_basement);
+        listDataChild.put(listDataHeader.get(1), R.drawable.memu_1);
+        listDataChild.put(listDataHeader.get(2), R.drawable.memu_2);
+        listDataChild.put(listDataHeader.get(3), R.drawable.memu_3);
+        listDataChild.put(listDataHeader.get(4), R.drawable.memu_4);
+    }
+}
---
+++ app/src/main/java/com/cs407/badgerbuddy/BuildingFloorListAdapter.java
@@ -0,0 +1,110 @@
+package com.cs407.badgerbuddy;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class BuildingFloorListAdapter extends BaseExpandableListAdapter {
+
+    private Context context;
+    private List<String> listDataHeader; // 그룹 항목
+    private HashMap<String, Integer> listDataChild;
+
+    public BuildingFloorListAdapter(Context context, List<String> listDataHeader,
+                                    HashMap<String, Integer> listDataChild) {
+        this.context = context;
+        this.listDataHeader = listDataHeader;
+        this.listDataChild = listDataChild;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return listDataHeader.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return 1; // 각 그룹당 하나의 하위 항목만 있음
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return listDataHeader.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return listDataChild.get(listDataHeader.get(groupPosition));
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        String headerTitle = (String) getGroup(groupPosition);
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_group, null);
+        }
+
+        TextView listTitleTextView = convertView.findViewById(R.id.listTitle);
+        listTitleTextView.setText(headerTitle);
+
+        ImageView groupIndicatorImageView = convertView.findViewById(R.id.groupIndicator);
+
+        listTitleTextView.setText(headerTitle);
+
+        // for the backarrow to be on the right
+        groupIndicatorImageView.setImageResource(isExpanded ? R.drawable.baseline_expand_less_black : R.drawable.baseline_expand_more_black);
+
+
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        int imageResource = (Integer) getChild(groupPosition, childPosition);
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.build_floor_list_item, null);
+        }
+
+        ImageView expandedListImageView = convertView.findViewById(R.id.expandedListItem);
+        expandedListImageView.setImageResource(imageResource);
+//        expandedListImageView.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
